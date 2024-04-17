1b129563628c9d9f7b2282fb7fe1f4124be6d671
Thomatito <zarama.minecraft@gmail.com>
2024-04-17 14:56:55 -0300

    att 17/04

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 import { useState } from "react";
+import { styles } from "../config/styles";
 
 export default function RegisterScreen({ navigation }) {
   const [email, setEmail] = useState("");
@@ -15,22 +16,61 @@ export default function RegisterScreen({ navigation }) {
   // Endereço: Logradouro, CEP, Cidade, Estado
   // O que é LOGRADOURO? É um termo que designa um terreno ou um espaço anexo a uma habitação, usado para serventia da casa, ou ainda qualquer espaço público comum que pode ser usufruído por toda a população e reconhecido pela administração de um município, como largos, praças, ruas, jardins, parques, entre outros.
   return (
-    <View>
-      <Text>Faça seu Registro</Text>
-      <TextInput
-        placeholder="Digite seu nome"
-        value={nome}
-        onChangeText={setNome}
-      />
-      <TextInput
-        placeholder="Digite seu email"
-        value={email}
-        onChangeText={setEmail}
-      />
-
-      <Button onPress={() => navigation.navigate("LoginScreen")}>
-        Voltar ao login
-      </Button>
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text>Faça seu Registro</Text>
+        <TextInput
+          placeholder="Digite seu nome"
+          value={nome}
+          onChangeText={setNome}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Digite seu email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Repita sua senha"
+          value={repetirSenha}
+          onChangeText={setRepetirSenha}
+          secureTextEntry
+          style={styles.input}
+        />
+        <View
+          style={{
+            paddingVertical: 20,
+          }}
+        >
+          <Text>Dados pessoais</Text>
+          <TextInput
+            placeholder="Digite seu CEP"
+            value={cep}
+            onChangeText={setCep}
+          />
+          <TextInput
+            placeholder="Logradouro"
+            value={logradouro}
+            onChangeText={setLogradouro}
+          />
+          <TextInput
+            placeholder="Cidade"
+            value={cidade}
+            onChangeText={setCidade}
+          />
+        </View>
+        <Button onPress={() => navigation.navigate("LoginScreen")}>
+          Voltar ao login
+        </Button>
+      </View>
     </View>
   );
 }
